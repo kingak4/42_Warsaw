8ee7833d471a90ef99528a7dd9a3275efd7e111f
root <root@LAPTOP-JL5ITJ3U.localdomain>
2025-03-07 20:53:26 +0800

    starting pipex

---
+++ pipex/pipex.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 22:06:31 by root              #+#    #+#             */
+/*   Updated: 2025/03/07 20:48:57 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+void	exit_f(int exit_type);
+
+
+#endif
