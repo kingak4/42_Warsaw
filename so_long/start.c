eded33996c1fb2d64620b31b981b534c24686d81
kinga <kinga.kwasniak5@gmail.com>
2025-05-05 17:15:01 +0200

    open window

--- so_long/start.c
+++ so_long/start.c
@@ -1,29 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.h                                          :+:      :+:    :+:   */
+/*   start.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kikwasni <kikwasni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/24 12:34:34 by kikwasni          #+#    #+#             */
-/*   Updated: 2025/05/05 09:50:27 by kikwasni         ###   ########.fr       */
+/*   Created: 2025/05/05 09:55:42 by kikwasni          #+#    #+#             */
+/*   Updated: 2025/05/05 17:13:21 by kikwasni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SO_LONG_H
-# define SO_LONG_H
+#include "so_long.h"
+#include <stdio.h>
 
-# include "./mlx/mlx.h"
-# include "./libft/libft.h"
-
-typedef struct s_so_long
+void	open_window(t_so_long *game)
 {
-	void *mlx;
-	void *win;
-	char *map;
-	int player_x;
-	int player_y;
-	int moves;
-} t_so_long;
-
-#endif
+	game->mlx = mlx_init();
+	printf("Otwarcie okna...\n");
+	game->win = mlx_new_window(game->mlx,832,640, "so_long");
+	printf("Otwarcie okna...\n");
+	mlx_loop(game->mlx);
+}
+int main()
+{
+	t_so_long game;
+	
+	open_window(&game);
+}
--- so_long/test.c
+++ so_long/test.c
@@ -6,7 +6,7 @@
 /*   By: kikwasni <kikwasni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:10:00 by kikwasni          #+#    #+#             */
-/*   Updated: 2025/04/30 12:11:56 by kikwasni         ###   ########.fr       */
+/*   Updated: 2025/05/05 09:39:15 by kikwasni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int close_window(void *param)
-{
-    mlx_destroy_window(param, param);
-    exit(0);
-}
-int	hokkey(int x ,void *win, void *mlx)
-{ 
-	int y = 32;
-	while (y <= height)
-	{
-		mlx_put_image_to_window(mlx, win, bird_img,x,y);
-		y += 32;
-		printf("%d", y);
-	}
-}
-int main(void)
-{
-    void *mlx;
-    void *win;
-    int width = 800;
-    int height = 600;
-    void *bird_img;
-	int x = 100;
-	int y = 32;
+//int close_window(void *param)
+//{
+//    mlx_destroy_window(param, param);
+//    exit(0);
+//}
+//int	hokkey(int x ,void *win, void *mlx)
+//{ 
+//	int y = 32;
+//	while (y <= height)
+//	{
+//		mlx_put_image_to_window(mlx, win, bird_img,x,y);
+//		y += 32;
+//		printf("%d", y);
+//	}
+//}
+//int main(void)
+//{
+//    void *mlx;
+//    void *win;
+//    int width = 800;
+//    int height = 600;
+//    void *bird_img;
+//	int x = 100;
+//	int y = 32;
 	
 
-    mlx = mlx_init();
+//    mlx = mlx_init();
 
-    win = mlx_new_window(mlx, 800, 600, "Bird Test");
+//    win = mlx_new_window(mlx, 800, 600, "Bird Test");
 
-    bird_img = mlx_xpm_file_to_image(mlx, "bird.xpm", &width, &height);
+//    bird_img = mlx_xpm_file_to_image(mlx, "bird.xpm", &width, &height);
 
 	
-	mlx_loop_hook()
-    //mlx_hook(win, 17, 0, close_window, mlx); // Zamyka okno przy kliknięciu w krzyżyk
-    //return (0);
-}
+//	mlx_loop_hook()
+//    //mlx_hook(win, 17, 0, close_window, mlx); // Zamyka okno przy kliknięciu w krzyżyk
+//    //return (0);
+//}
 
 //int main()
 //{
