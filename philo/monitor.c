8add5151f8e33c8fb39908d55b6a7ab2e5a01fdb
kinga <kinga.kwasniak5@gmail.com>
2025-08-21 12:18:15 +0200

    norma in routina and 5 800 200 200 7 test

--- philo/monitor.c
+++ philo/monitor.c
@@ -6,80 +6,62 @@
 /*   By: kikwasni <kikwasni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/09 00:57:38 by root              #+#    #+#             */
-/*   Updated: 2025/08/21 10:20:48 by kikwasni         ###   ########.fr       */
+/*   Updated: 2025/08/21 12:11:43 by kikwasni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-//void *monitor(void *arg)
-//{
-//	t_args *data;
-//	int	i;
+static int	check_death(t_args *data)
+{
+	int		i;
+	long	current_time;
 
-//	data = (t_args *)arg;
-//	while (1)
-//	{
-//		i = 0;
-//		while (i < data->philo_count)
-//		{
-//			pthread_mutex_lock(&data->philo[i].meal_mutex);
-//			if (data->philo[i].died)
-//			{
-//				pthread_mutex_unlock(&data->philo[i].meal_mutex);
-//				pthread_mutex_lock(&data->print_mutex);
-//				printf("Philosopher %d died\n", data->philo[i].id);
-//				pthread_mutex_unlock(&data->print_mutex);
-//				return NULL;
-//			}
-//			pthread_mutex_unlock(&data->philo[i].meal_mutex);
-//			i++;
-//		}
-//		if (all_philos_eaten(data))
-//		{
-//			pthread_mutex_lock(&data->print_mutex);
-//			pthread_mutex_unlock(&data->print_mutex);
-//			return NULL;
-//		}
-//		usleep(1000);
-//	}
-//	return (NULL);
-//}
+	i = 0;
+	while (i < data->philo_count)
+	{
+		pthread_mutex_lock(&data->philo[i].meal_mutex);
+		current_time = get_current_time();
+		if (!data->philo[i].died
+			&& (current_time - data->philo[i].last_meal > data->time_to_die))
+		{
+			pthread_mutex_unlock(&data->philo[i].meal_mutex);
+			pthread_mutex_lock(&data->death);
+			data->philo[i].died = 1;
+			pthread_mutex_unlock(&data->death);
+			pthread_mutex_lock(&data->print_mutex);
+			printf("%ld %d died\n", get_relative_time(data), data->philo[i].id);
+			pthread_mutex_unlock(&data->print_mutex);
+			return (1);
+		}
+		pthread_mutex_unlock(&data->philo[i].meal_mutex);
+		i++;
+	}
+	return (0);
+}
+
+static int	check_meals(t_args *data)
+{
+	if (data->must_eat_count > 0 && all_philos_eaten(data))
+	{
+		pthread_mutex_lock(&data->print_mutex);
+		pthread_mutex_unlock(&data->print_mutex);
+		return (1);
+	}
+	return (0);
+}
 
 void	*monitor(void *arg)
 {
-	t_args *data;
-	int	i;
-	long	current_time;
+	t_args	*data;
 
 	data = (t_args *)arg;
 	while (1)
 	{
-		i = 0;
-		while (i < data->philo_count)
-		{
-			pthread_mutex_lock(&data->philo[i].meal_mutex);
-			current_time = get_current_time();
-			if (!data->philo[i].died && (current_time - data->philo[i].last_meal > data->time_to_die))
-			{
-				pthread_mutex_unlock(&data->philo[i].meal_mutex);
-				pthread_mutex_lock(&data->death);
-				data->philo[i].died = 1;
-				pthread_mutex_unlock(&data->death);
-				pthread_mutex_lock(&data->print_mutex);
-				printf("%ld %d died\n", get_relative_time(data), data->philo[i].id);
-				pthread_mutex_unlock(&data->print_mutex);
-				return (NULL);
-			}
-			pthread_mutex_unlock(&data->philo[i].meal_mutex);
-			i++;
-		}
-		if (data->must_eat_count > 0 && all_philos_eaten(data))
-		{
-			pthread_mutex_lock(&data->print_mutex);
-			pthread_mutex_unlock(&data->print_mutex);
+		if (check_death(data))
 			return (NULL);
-		}
+		if (check_meals(data))
+			return (NULL);
 		usleep(10000);
 	}
 	return (NULL);
--- philo/routine.c
+++ philo/routine.c
@@ -6,13 +6,13 @@
 /*   By: kikwasni <kikwasni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/07 15:16:18 by kikwasni          #+#    #+#             */
-/*   Updated: 2025/08/21 10:20:44 by kikwasni         ###   ########.fr       */
+/*   Updated: 2025/08/21 12:17:17 by kikwasni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int philo_take_forks(t_philo *philo)
+int	philo_take_forks(t_philo *philo)
 {
 	if (philo->left_fork > philo->right_fork)
 	{
@@ -52,13 +52,14 @@ int philo_take_forks(t_philo *philo)
 	}
 	return (0);
 }
-void philo_sleep(t_philo *philo)
+
+void	philo_sleep(t_philo *philo)
 {
 	print_action(philo, "is sleeping");
 	ft_usleep(philo->args->time_to_sleep);
 }
 
-void philo_think(t_philo *philo)
+void	philo_think(t_philo *philo)
 {
 	print_action(philo, "is thinking");
 }
@@ -67,7 +68,7 @@ void philo_think(t_philo *philo)
 //{
 //	if (philo_take_forks(philo))
 //		return (1);
-//	print_action(philo, "is eating"); //dodaj flage ze jak je t ni e moze umierac
+//	print_action(philo, "is eating");
 //	ft_usleep(args->time_to_eat);
 //	pthread_mutex_lock(&philo->meal_mutex);
 //	philo->last_meal = get_current_time();
@@ -95,8 +96,8 @@ int philo_eat(t_philo *philo, t_args *args)
 
 void *philo_routine(void *arg)
 {
-	t_philo *p = (t_philo *)arg;
-	t_args *a = p->args;
+	t_philo	*p = (t_philo *)arg;
+	t_args	*a = p->args;
 
 	if (p->id % 2 != 0)
 		ft_usleep(1);
@@ -105,16 +106,11 @@ void *philo_routine(void *arg)
 		if (is_any_philo_dead(a) == 1)
 			return (NULL);
 		if (philo_eat(p, a))
-		{
-			// pthread_mutex_unlock(p->left_fork);
-			// pthread_mutex_unlock(p->right_fork);
 			return (NULL);
-		}
 		if (is_any_philo_dead(a) == 1)
 			return (NULL);
-		if ((a->must_eat_count > 0 && p->eat_count >= a->must_eat_count)){
-			break;
-		}
+		if ((a->must_eat_count > 0 && p->eat_count >= a->must_eat_count))
+			break ;
 		philo_sleep(p);
 		if (is_any_philo_dead(a) == 1)
 			return (NULL);
